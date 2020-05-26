80df73abe34aedca43122631f71a51b331c5221c
Catherine L <liucatherine97@gmail.com>
2020-05-26 11:59:07 -0400

    Reformat title and add appropriate checkboxes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def res():
 
       # dummy bokeh
       r = requests.get('https://www.alphavantage.co/query?',
-                       params={'function': 'TIME_SERIES_DAILY', 'symbol': stock, 'apikey': 'demo'})
+                       params={'function': 'TIME_SERIES_DAILY_ADJUSTED', 'symbol': stock, 'apikey': 'demo'})
       ex = r.json()
       df = pd.DataFrame.from_dict(ex["Time Series (Daily)"])
       df = df.T
@@ -36,8 +36,8 @@ def res():
           y=pd.to_numeric(df['4. close'])))
 
       p = figure(plot_width=400, plot_height=400, x_axis_type='datetime', tools='pan,wheel_zoom,box_select,reset')
-      # add a line renderer
 
+      # add a line renderer
       p.line('x', 'y', source=df_src)
       plot_script, plot_div = components(p)
 
