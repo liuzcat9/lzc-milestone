016c8af578cfe5839a2c79684a99e9fdfb4a55d2
Catherine L <liucatherine97@gmail.com>
2020-06-08 14:13:37 -0400

    Create base template and modify bootstrap

--- app.py
+++ app.py
@@ -12,44 +12,48 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
 
-@app.route('/res',methods = ['POST', 'GET'])
+
+@app.route('/res', methods=['POST', 'GET'])
 def res():
-   if request.method == 'POST':
-      stock = request.form.getlist('stockname')
-      result = request.form.getlist('check')
+    if request.method == 'POST':
+        stock = request.form.getlist('stockname')
+        result = request.form.getlist('check')
 
-      # bokeh using API results
-      r = requests.get('https://www.alphavantage.co/query?',
-                       params={'function': 'TIME_SERIES_DAILY_ADJUSTED', 'symbol': stock, 'apikey': os.getenv('ALPHA_API')})
-      ex = r.json()
-      df = pd.DataFrame.from_dict(ex["Time Series (Daily)"])
-      df = df.T
+        # bokeh using API results
+        r = requests.get('https://www.alphavantage.co/query?',
+                         params={'function': 'TIME_SERIES_DAILY_ADJUSTED', 'symbol': stock[0],
+                                 'apikey': os.getenv('ALPHA_API')})
+        ex = r.json()
+        df = pd.DataFrame.from_dict(ex["Time Series (Daily)"])
+        df = df.T
 
-      # convert data types in dataframe
-      df.index = pd.to_datetime(df.index)
-      df = df.apply(pd.to_numeric, axis=1)
+        # convert data types in dataframe
+        df.index = pd.to_datetime(df.index)
+        df = df.apply(pd.to_numeric, axis=1)
 
-      p = figure(plot_width=600, plot_height=400, x_axis_type='datetime', tools='pan,wheel_zoom,box_select,reset')
+        p = figure(plot_width=600, plot_height=400, title="Ticker Lookup for " + stock[0], x_axis_label='Date', x_axis_type='datetime',
+                   tools='pan, wheel_zoom, box_zoom, reset, save')
 
-      # add a line renderer for each selected group
-      colors = ["navy", "green", "firebrick", "grey"]
-      legend_list = []
-      for i, result_type in enumerate(result):
-          item = p.line(df.index, df[result_type], color = colors[i])
-          legend_list.append((result_type[2:], [item]))
+        # add a line renderer for each selected group
+        colors = ["navy", "green", "firebrick", "grey"]
+        legend_list = []
+        for i, result_type in enumerate(result):
+            item = p.line(df.index, df[result_type], color=colors[i])
+            legend_list.append((result_type[2:], [item]))
 
-      legend = Legend(items = legend_list)
-      p.add_layout(legend, 'right')
+        legend = Legend(items=legend_list)
+        p.add_layout(legend, 'right')
 
-      plot_script, plot_div = components(p)
+        plot_script, plot_div = components(p)
 
-      return render_template("res.html", result = result, bscript = plot_script, bdiv = plot_div)
-
+        return render_template("res.html", bscript=plot_script, bdiv=plot_div)
