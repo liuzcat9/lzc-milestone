c900d52a0ec4aded1b8dfbf714b27b53e715351a
Catherine L <liucatherine97@gmail.com>
2020-06-08 12:18:26 -0400

    Move legend

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 
 import bokeh
 from bokeh.plotting import figure, show
-from bokeh.models import ColumnDataSource
 from bokeh.embed import components
+from bokeh.models import Legend
 
 import os
 
@@ -28,7 +28,7 @@ def res():
 
       # bokeh using API results
       r = requests.get('https://www.alphavantage.co/query?',
-                       params={'function': 'TIME_SERIES_DAILY_ADJUSTED', 'symbol': stock, 'apikey': os.environ['ALPHA_API']})
+                       params={'function': 'TIME_SERIES_DAILY_ADJUSTED', 'symbol': stock, 'apikey': os.getenv('ALPHA_API')})
       ex = r.json()
       df = pd.DataFrame.from_dict(ex["Time Series (Daily)"])
       df = df.T
@@ -37,12 +37,17 @@ def res():
       df.index = pd.to_datetime(df.index)
       df = df.apply(pd.to_numeric, axis=1)
 
-      p = figure(plot_width=400, plot_height=400, x_axis_type='datetime', tools='pan,wheel_zoom,box_select,reset')
+      p = figure(plot_width=600, plot_height=400, x_axis_type='datetime', tools='pan,wheel_zoom,box_select,reset')
 
       # add a line renderer for each selected group
       colors = ["navy", "green", "firebrick", "grey"]
+      legend_list = []
       for i, result_type in enumerate(result):
-          p.line(df.index, df[result_type], color = colors[i], legend_label = result_type[2:])
+          item = p.line(df.index, df[result_type], color = colors[i])
+          legend_list.append((result_type[2:], [item]))
+
+      legend = Legend(items = legend_list)
+      p.add_layout(legend, 'right')
 
       plot_script, plot_div = components(p)
 
